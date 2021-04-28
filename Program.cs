b0cffae5c3e7cd12ab632635be13fca29e993195
Rawabe Ebrahim <rawabeebrahim@Rawabes-MacBook-Pro.local>
2021-04-28 16:46:07 +0300

    create subclass and impleement abstract sum method

--- Program.cs
+++ Program.cs
@@ -6,11 +6,21 @@ namespace GitPractice
 
         public abstract int sum(int a,int b);
     }
+
+    class SubPractice : practice
+    {
+        public override int sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Sum of two intgers: ");
         }
     }
 }
